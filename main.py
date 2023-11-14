a463d17567184d5309598452998069747d0b7b77
godinlu <luc.villaroux@orange.fr>
2023-11-14 15:14:56 +0100

    ajout de la première solution pour une carte

---
+++ main.py
@@ -0,0 +1,14 @@
+import folium
+
+# Coordonnées centrales de la région Auvergne-Rhône-Alpes
+latitude = 45.75
+longitude = 4.85
+
+# Créer la carte centrée sur la région
+ma_carte = folium.Map(location=[latitude, longitude], zoom_start=7)
+
+# Ajouter un marqueur pour indiquer le centre
+folium.Marker([latitude, longitude], popup='Auvergne-Rhône-Alpes').add_to(ma_carte)
+
+# Enregistrez la carte au format HTML
+ma_carte.save('carte_auvergne_rhone_alpes.html')
