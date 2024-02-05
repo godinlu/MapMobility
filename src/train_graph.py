cf20e8acbf0a7dbed56075608b4dcb39c93b2a59
godinlu <luc.villaroux@orange.fr>
2024-02-05 11:52:14 +0100

    commit des changement

--- src/train_graph.py
+++ src/train_graph.py
@@ -6,6 +6,9 @@ from .utils import heures_en_secondes
 
 class TrainGraph:
     def __init__(self, df_stops_times:pd.DataFrame) -> None:
+        """
+        Lorsque l'on créer 
+        """
         self.graph = nx.MultiDiGraph()
 
         for _, trip in tqdm(df_stops_times.groupby('trip_id'), desc="Processing trips"):
@@ -16,28 +19,25 @@ class TrainGraph:
                 time_difference = heures_en_secondes(next_stop['arrival_time']) -  heures_en_secondes(first_stop['departure_time'])
                 self.graph.add_edge(first_stop['stop_id'], next_stop['stop_id'], weight=time_difference)
 
+    def get_shortest_path(self, id_stop_start:str, id_stop_end:str)->list:
+        """
+        Cette fonction renvoie le chemin le plus court entre les deux gares
+        """
+        return nx.shortest_path(self.graph, id_stop_start, id_stop_end)
         
-    def get_shortest_path(self, id_stop_start:str, id_stop_end:str)->int:
+    def get_time_between(self, id_stop_start:str, id_stop_end:str)->int:
         """
         Cette fonction renvoie le temps en seconde du plus cours chemin entre 2 stop
         """
-        shortest_path = nx.shortest_path(self.graph, id_stop_start, id_stop_end)
+        shortest_path = self.get_shortest_path(id_stop_start, id_stop_end)
         cumulative_weight = sum(self.graph[shortest_path[i]][shortest_path[i+1]][0]['weight'] for i in range(len(shortest_path)-1))
         return cumulative_weight
+    
+    def get_dijkstra(self, id_source_stop:str):
+        return nx.single_source_dijkstra_path_length(self.graph, id_source_stop, weight='weight')
 
 
-    def show(self)->None:
-        # Visualisation du graphe
-        pos = nx.spring_layout(self.graph)  # Vous pouvez choisir un autre algorithme de disposition
-        nx.draw(self.graph ,pos)
-        # Ajout des étiquettes des arêtes directement sur le graphique
-        labels = nx.get_edge_attributes(self.graph, 'weight')
-        for edge, weight in labels.items():
-            x, y = pos[edge[0]]
-            label_x = (x + pos[edge[1]][0]) / 2
-            label_y = (y + pos[edge[1]][1]) / 2
-            plt.text(label_x, label_y, f"{weight:.2f}", fontsize=8, color='red')
 
-        plt.show()
+
 
         
